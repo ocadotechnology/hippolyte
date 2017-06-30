b74ffaf8fd82d88a7cd1613404a9a51c88dec860
Roman Subik <roman.subik@ocado.com>
2017-06-30 23:30:49 +0100

    #4 Fail Travis build, if tests fail.

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,9 @@
 import unittest
+import sys
 
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
     suite = unittest.TestLoader().discover('.')
-    runner.run(suite)
+    exit_code = not runner.run(suite).wasSuccessful()
+    sys.exit(exit_code)
